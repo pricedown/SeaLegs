c766b87e66b46b54069444229dbf1ff522a1a4a9
pricedown <josephmisaacs@gmail.com>
2025-12-06 21:52:17 -0500

    HullTriangle
    A triangle on the boat with computed properties

---
+++ Assets/Scripts/HullTriangle.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+namespace SeaLegs
+{
+    /// <summary>
+    /// A triangle on the boat hull with computed properties
+    /// - three vertices
+    /// - cached center,
+    /// - cached normal
+    /// - unitnormal,
+    /// - area
+    /// Foundation for Kerner's approach to buoyancy
+    /// https://www.gamedeveloper.com/programming/water-interaction-model-for-boats-in-video-games
+    /// </summary>
+    public struct HullTriangle
+    {
+        public Vector3 v0, v1, v2;
+        public Vector3 Center => (v0 + v1 + v2) / 3f;
+        public Vector3 Normal => Vector3.Cross(v1 - v0, v2 - v0);
+        public Vector3 UnitNormal => Vector3.Normalize(Normal);
+        public float Area => Normal.Length() * 0.5f;
+
+        public HullTriangle(Vector3 v0, Vector3 v1, Vector3 v2)
+        {
+            this.v0 = v0;
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+    }
+}
